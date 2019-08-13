e2da4aeed65da27048dd7dfc77b3dfdc1fde1f45
ohsung cho <fifthsage@gmail.com>
2019-08-14 00:08:37 +0900

    dist

---
+++ dist/test-helper.js
@@ -0,0 +1,2 @@
+"use strict";var _interopRequireDefault=require("@babel/runtime/helpers/interopRequireDefault");var _enzyme=require("enzyme");var _enzymeAdapterReact=_interopRequireDefault(require("enzyme-adapter-react-16"));(0,_enzyme.configure)({adapter:new _enzymeAdapterReact["default"]()});
+//# sourceMappingURL=test-helper.js.map
---
+++ dist/variables/colors.js
@@ -0,0 +1,2 @@
+"use strict";Object.defineProperty(exports,"__esModule",{value:true});exports["default"]=void 0;var _default={primary:'#2089dc',secondary:'#8F0CE8',success:'#52c41a',error:'#ff190c',warning:'#faad14',grey0:'#393e42',grey1:'#43484d',grey2:'#5e6977',grey3:'#86939e',grey4:'#bdc6cf',grey5:'#e1e8ee'};exports["default"]=_default;
+//# sourceMappingURL=colors.js.map
