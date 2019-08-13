b0f308c5913cac98cd0a2aa559b342b469432c20
ohsung cho <fifthsage@gmail.com>
2019-08-13 15:02:01 +0900

    dist include

---
+++ dist/variables/fonts.js
@@ -0,0 +1,40 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+var _default = {
+  android: {
+    regular: {
+      fontFamily: 'sans-serif'
+    },
+    light: {
+      fontFamily: 'sans-serif-light'
+    },
+    condensed: {
+      fontFamily: 'sans-serif-condensed'
+    },
+    condensed_light: {
+      fontFamily: 'sans-serif-condensed',
+      fontWeight: 'light'
+    },
+    black: {
+      fontFamily: 'sans-serif',
+      fontWeight: 'bold'
+    },
+    thin: {
+      fontFamily: 'sans-serif-thin'
+    },
+    medium: {
+      fontFamily: 'sans-serif-medium'
+    },
+    bold: {
+      fontFamily: 'sans-serif',
+      fontWeight: 'bold'
+    }
+  },
+  "default": {}
+};
+exports["default"] = _default;
+//# sourceMappingURL=fonts.js.map
